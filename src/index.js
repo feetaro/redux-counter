30fa3a2eebd44c833eabcf8070abe53d579342c3
feetaro <feetaro@gmail.com>
2021-10-05 13:50:22 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import reducer from "./reducers";
+import App from "./components/App";
+
+const store = createStore(reducer);
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
